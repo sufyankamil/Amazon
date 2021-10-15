6161e2bd0c606547cf2e5a827c81cbab6384b947
Sufyan Kamil <sufyankamil15@gmail.com>
2021-10-15 22:04:24 +0530

    init

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import "./Subtotal.css";
+
 
 // eslint-disable-next-line no-unused-vars
 function Subtotal() {
     return (
         <div className="subtotal">
-            <p>Subtotal (0 items):</p> <strong>0</strong>
+            <p>Subtotal (0 items): <strong>0</strong></p>
+            <small className="subtotal--gift">
+                <input type="checkbox" /> This order contains a gift
+            </small>
+
+            <button className="">Proceed to Checkout</button>
+
 
         </div>
     );
