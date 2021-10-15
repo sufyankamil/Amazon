959513b653e3009315571bd7b72b94736ab03544
Sufyan Kamil <sufyankamil15@gmail.com>
2021-10-15 21:22:44 +0530

    init

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
+
 function Checkout() {
     return (
         <div className="checkout">
-            <div className="checkout__left">
-            <img className="checkout__img" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="Loading Failed"/>
+            <div className="checkout--left">
+                <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
                 <div>
-                    <h3 className="checkout__title">Your Shopping Details</h3>
+                    <h3 className="checkout--title">Your Shopping Details</h3>
+                    {/* Cart Item here */}
+                    {/* Cart Item here */}
                 </div>
             </div>
 
-            <div className="checkout__left">
-                <h3>SubTotal (2 item):</h3>
+            <div className="checkout--right">
+                <Subtotal />
+
             </div>
         </div>
-    )
+    );
 }
 
-export default Checkout
+export default Checkout;
---
+++ src/Subtotal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// eslint-disable-next-line no-unused-vars
+function Subtotal() {
+    return (
+        <div className="subtotal">
+            <p>Subtotal (0 items):</p> <strong>0</strong>
+
+        </div>
+    );
+}
+
+export default Subtotal;
