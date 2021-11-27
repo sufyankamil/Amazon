ca0f76411a1382e56a95c09d9e6d3587bce76223
Sufyan Kamil <sufyankamil15@gmail.com>
2021-11-27 22:14:08 +0530

    updating

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+    cart: [],
+};
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case "ADD_TO_CART":
+            return{
+                // spread operator (...)
+                ...state,
+                cart: [...state.cart, action.item],
+            };
+            default:
+                return state;
+    }
+};
+
+export default reducer;
