99fa42e119b03b0b195d60e30ccb904dbaa86c02
Sufyan Kamil <sufyankamil15@gmail.com>
2021-12-05 21:18:49 +0530

    working on cart

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import "./Subtotal.css";
-
+import { useStateValue } from "./StateProvider";
+import CurrencyFormat from 'react-currency-format';
 
 // eslint-disable-next-line no-unused-vars
 function Subtotal() {
+    const [{ cart }, dispatch] = useStateValue();
+
     return (
         <div className="subtotal">
-            <p>Subtotal (0 items): <strong>0</strong></p>
-            <small className="subtotal--gift">
-                <input type="checkbox" /> This order contains a gift
-            </small>
+
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+
+
+                        <p>Subtotal ({cart.length} items): <strong>0</strong></p>
+                        <small className="subtotal--gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+
+            />
 
             <button className="">Proceed to Checkout</button>
-
-
         </div>
     );
 }
