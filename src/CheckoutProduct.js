7795cbb62da68d372fd4c488c7bc71262fc6f4e9
Sufyan Kamil <sufyankamil15@gmail.com>
2021-12-18 20:42:34 +0530

    checkout update

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './CheckoutProduct.css';
 
-function CheckoutProduct({id, title, price, image }) {
+function CheckoutProduct({ id, title, price, image }) {
     return (
         <div className='checkoutProduct'>
-            <img className='checkoutProductImage' src={image} alt=''/>
+            <img className='checkoutProductImage' src={image} alt='' />
 
             <div className='checkoutProduct--info'>
                 <p className='checkoutProduct--title'>{title}</p>
                 <p className='checkoutProduct--price'>
-                <bold>₹</bold>
-                <strong>{price}</strong>
+                    <bold>₹</bold>
+                    <strong>{price}</strong>
                 </p>
             </div>
-            <button>Remove From Cart</button>
+            <button className='product--button--exit'>Remove From Cart</button>
         </div>
     )
 }
