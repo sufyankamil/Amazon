86e8d4e483cd1d5ae91c0c091a8411076bbbe540
Sufyan Kamil <sufyankamil15@gmail.com>
2022-01-23 20:14:07 +0530

    Adding and updates to  payment gateway

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 // import { Router } from 'react-router';
 // eslint-disable-next-line no-unused-vars
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-import Payment  from "./Payment";
+import Payment from "./Payment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
-import {auth} from "./firebase";
+import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51KIbbuSDUComm5F9vFvuvRbjTB8OdlGEls8x4bi2N1CmfETkWWM2ekdBY5aZ6NvdJ3o3ujxfNb0f1VIrJAJ7WtBi00YgQzqIFk"
+);
 
 function App() {
-  const [{user, setUser}, dispatch] = useStateValue(null);
-
+  const [{ user, setUser }, dispatch] = useStateValue(null);
 
   useEffect(() => {
     // will run when the app component is mounted
-    auth.onAuthStateChanged(
-      authUser => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS: ", authUser);
 
-        console.log("THE USER IS: ", authUser);
-
-        if (authUser) {
-          // user logged in
-          dispatch(
-            // dispatch the user(creating object {})
-            {
-              type: "SET_USER",
-              user: authUser
-            }
-          )
-        } else {
-          // user not logged in
-          dispatch(
-            {
-              type: "SET_USER",
-              user: null
-            }
-          )
-        }
+      if (authUser) {
+        // user logged in
+        dispatch(
+          // dispatch the user(creating object {})
+          {
+            type: "SET_USER",
+            user: authUser,
+          }
+        );
+      } else {
+        // user not logged in
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
       }
-    )
-  }, [])
+    });
+  }, []);
 
   return (
     <Router>
@@ -57,7 +57,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
 
           <Route path="/">
--- src/Payment.js
+++ src/Payment.js
@@ -1,37 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Payment.css";
+import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import { getCartTotal } from "./reducer";
+import CurrencyFormat from "react-currency-format";
 
 function Payment() {
-    const [{ cart, user }, dispatch] = useStateValue();
+  const [{ cart, user }, dispatch] = useStateValue();
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+
+  const handleSubmit = (e) => {};
+
+  const handleChange = (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
   return (
     <div className="payment">
       <div className="payment--container">
         {/* Delivery Address */}
         <div className="payment--section">
+          <div className="payment--title">
             <h3>Delivery Address</h3>
-            <div className="payment--address">
-                <p>{user?.email}</p>
-                <p>123, ABC Street</p>
-                <p>New York, NY</p>
-            </div>
+          </div>
+          <div className="payment--address">
+            <p>{user?.email}</p>
+            <p>123, ABC Street</p>
+            <p>New York, NY</p>
+          </div>
         </div>
-        
+
         {/* Review Items */}
         <div className="payment--section">
+          <div className="payment--title">
             <h3>Review Items and Delivery</h3>
-                
+          </div>
+          <div className="payment--items">
+            {cart.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+              />
+            ))}
+          </div>
         </div>
-        
+
         {/* Payment Method */}
         <div className="payment--section">
+          <div className="payment--title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment--method">
+            <div className="payment--details">
+              {/*Stripe Secret Code */}
+              <form onSubmit={handleSubmit}>
+                <CardElement onChange={handleChange} />
 
+                <div className="payment--priceContainer">
+                  <CurrencyFormat
+                    renderText={(value) => (
+                      <>
+                        <p>
+                          Subtotal ({cart.length} items):
+                          <strong>{value}</strong>{" "}
+                        </p>
+                        <small className="subtotal--gift">
+                          <input type="checkbox" /> This order contains a gift
+                        </small>
+                      </>
+                    )}
+                    decimalScale={2}
+                    value={getCartTotal(cart)}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"₹"}
+                  />
+                  <button disabled={processing || disabled || succeeded}>
+                    <span> {processing ? <p>Processing</p> : "Buy Now"} </span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        
+
         {/* Place Order */}
-        <div className="payment--section">
-
-        </div>
+        <div className="payment--section"></div>
       </div>
     </div>
   );
